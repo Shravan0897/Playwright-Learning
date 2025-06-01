bdf1b51d75bd9bbe743f5a752831749e01528876
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2025-06-01 22:39:19 +0530

    feature: example login page added

---
+++ tests/login.spec.js
@@ -0,0 +1,11 @@
+const { test, expect } = require('@playwright/test')
+
+test("Verify Login", async function ({ page }) {
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
+    await expect(page).toHaveURL(/login/)
+    await page.getByPlaceholder("Username").fill("Admin")
+    await page.locator("input[placeholder='Password']").fill("admin123")
+    await page.locator("button[type='submit']").click()
+    await expect(page).toHaveURL(/dashboard/)
+
+})
