dc34f287fe471acbc9044033772a1ed94e579124
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2025-06-02 13:52:13 +0530

    feature: dashboard and logout changes

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -7,5 +7,7 @@ test("Verify Login", async function ({ page }) {
     await page.locator("input[placeholder='Password']").fill("admin123")
     await page.locator("button[type='submit']").click()
     await expect(page).toHaveURL(/dashboard/)
-
+    await page.getByAltText("profile picture").first().click()
+    await page.getByText("Logout").click()
+    await expect(page).toHaveURL(/login/)
 })
