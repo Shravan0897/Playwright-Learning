9c628155e1e90c1c91f7b8705c406957007a58d5
Shravan Tomar <shravan@Shravans-MacBook-Air.local>
2025-06-07 22:38:00 +0530

    feature: login page comments added

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,13 +1,17 @@
-const { test, expect } = require('@playwright/test')
+const { test, expect } = require('@playwright/test') // Import Playwright test and expect functions
 
-test("Verify Login", async function ({ page }) {
-    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login")
-    await expect(page).toHaveURL(/login/)
-    await page.getByPlaceholder("Username").fill("Admin")
-    await page.locator("input[placeholder='Password']").fill("admin123")
-    await page.locator("button[type='submit']").click()
-    await expect(page).toHaveURL(/dashboard/)
-    await page.getByAltText("profile picture").first().click()
-    await page.getByText("Logout").click()
-    await expect(page).toHaveURL(/login/)
+test.use({ viewport: { width: 375, height: 600 } }) // Set the viewport size to simulate a mobile device
+
+test("Verify Login", async function ({ page }) { // Define a test case named "Verify Login"
+    await page.goto("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login") // Navigate to the login page
+    console.log(await page.viewportSize().width) // Log the current viewport width
+    console.log(await page.viewportSize().height) // Log the current viewport height
+    await expect(page).toHaveURL(/login/) // Assert that the URL contains 'login'
+    await page.getByPlaceholder("Username").fill("Admin") // Fill the username field with 'Admin'
+    await page.locator("input[placeholder='Password']").fill("admin123") // Fill the password field with 'admin123'
+    await page.locator("button[type='submit']").click() // Click the login button
+    await expect(page).toHaveURL(/dashboard/) // Assert that the URL contains 'dashboard' after login
+    await page.getByAltText("profile picture").first().click() // Click on the profile picture to open the menu
+    await page.getByText("Logout").click() // Click the 'Logout' option
+    await expect(page).toHaveURL(/login/) // Assert that the URL contains 'login' after logout
 })
